6a8bdb3911b83bb9637573df62683b6f0846abe4
Anthony Fecondo <afecondo@udel.edu>
2017-11-07 13:39:01 -0500

    flask script added

--- songbase.py
+++ songbase.py
@@ -3,12 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')     #to create route in Flask, use the @ sign
 def home():         #function
-    return '<h1>hello world!!!!</h1>'
+    return '<h1>hello world!!!! lalala </h1>'
 
-@app.route('/user/<string:name>/')     #defining a route with variable
+@app.route('/user/')     #defining a route with variable
 def get_user(name):
     return '<h1>hello %s your age is %d</h1>' % (name, 3)
 
 
 if __name__ == '__main__':      #creates an entry point for your program to run
-    app.run(debug=True)        #app.run(host='0.0.0.0', port=1216) to host publically, must use port larger than 1024
+    app.run()        #app.run(host='0.0.0.0', port=1216) to host publically, must use port larger than 1024

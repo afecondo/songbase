66b538ce3f91e1709af3052c4923439b3cf413de
Anthony Fecondo <afecondo@udel.edu>
2017-11-14 13:43:31 -0500

    in class exercise with flask and songbase

--- manage.py
+++ manage.py
@@ -1,7 +1,13 @@
 from flask_script import Manager
 from songbase import app
 
-manager = Manager(app)   ##create a manager that manages app
+manager = Manager(app)
 
-if __name__ == '__main__':
+
+@manager.command
+def hello():
+    print "hello"
+
+
+if __name__ == "__main__":
     manager.run()
--- songbase.py
+++ songbase.py
@@ -1,25 +1,72 @@
-from flask import Flask, render_template    #need these two lines
+from flask import Flask, session, render_template, request, flash, redirect, url_for
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to get secure key'
 
 
-@app.route('/')     #to create route in Flask, use the @ sign
-def index():         #function
-    #return '<h1>hello world!!!! lalala </h1>'
+@app.route('/')
+def index():
+    # return HTML
+    # return "<h1>this is the index page!<h1>"
     return render_template('index.html')
 
+
 @app.route('/songs')
-def get_all_songs():
+def show_all_songs():
     songs = [
-        'Baba O Reilly',
-        'Eh Cumpari',
-        'Fly me to the Moon'
+        'Paradise',
+        'Yellow',
+        'Viva La Vida'
     ]
-    return render_template('songs.html', songs=songs)
 
-@app.route('/user/<string:name>/')     #defining a route with variable
+    return render_template('song-all.html', songs=songs)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/users')
+def show_all_users():
+    return render_template('user-all.html')
+
+
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    # how to get form data is different for GET vs. POST
+    if request.method == 'GET':
+        first_name = request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html', first_name=first_name)
+        else:
+            return render_template('form-demo.html', first_name=session.get('first_name'))
+    if request.method == 'POST':
+        session['first_name'] = request.form['first_name']
+        # return render_template('form-demo.html', first_name=first_name)
+        return redirect(url_for('form_demo'))
+
+
+@app.route('/user/<string:name>/')
 def get_user_name(name):
-    #return '<h1>hello %s your age is %d</h1>' % (name, 3)
-    return render_template('user.html', user_name=name)
+    # return "hello " + name
+    # return "Hello %s, this is %s" % (name, 'administrator')
+    return render_template('user.html', name=name)
 
-if __name__ == '__main__':      #creates an entry point for your program to run
-    app.run()        #app.run(host='0.0.0.0', port=1216) to host publically, must use port larger than 1024
+
+@app.route('/song/<int:id>/')
+def get_song_id(id):
+    # return "This song's ID is " + str(id)
+    return "Hi, this is %s and the song's id is %d" % ('administrator', id)
+
+
+# https://goo.gl/Pc39w8 explains the following line
+if __name__ == '__main__':
+
+    # activates the debugger and the reloader during development
+    # app.run(debug=True)
+    app.run()
+
+    # make the server publicly available on port 80
+    # note that Ports below 1024 can be opened only by root
+    # you need to use sudo for the following conmmand
+    # app.run(host='0.0.0.0', port=80)
